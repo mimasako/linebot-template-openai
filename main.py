fe2d26b0dae28a98c524d9fe81304c069bbadcba
mimasako <seiya1012@me.com>
2025-07-17 19:18:54 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from fastapi import FastAPI, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
 @app.post("/webhook")
 async def callback(request: Request):
     signature = request.headers.get("X-Line-Signature", "")
